a332b6ca03ef0179eeaaecd13a7e613ecaf0587e
SoSo <sosoariar@163.com>
2024-01-31 20:56:49 +0800

    function toString()

--- datastruct/src/main/java/com/soso/Array.java
+++ datastruct/src/main/java/com/soso/Array.java
@@ -49,4 +49,18 @@ public class Array {
         add(size,0);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("size: %d, capacity %d\n",size,data.length));
+        res.append('[');
+        for(int i=0;i<size;i++){
+            res.append(data[i]);
+            if(i!=size-1){
+                res.append(",");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
 }
