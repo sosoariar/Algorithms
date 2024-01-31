31c26d3e84ef8331a0641a34cffe6ba8894a1285
SoSo <sosoariar@163.com>
2024-01-31 20:30:04 +0800

    array function add

---
+++ datastruct/src/main/java/com/soso/Array.java
@@ -0,0 +1,40 @@
+package com.soso;
+
+public class Array {
+
+    private int[] data;
+    private int size;
+
+    public Array(int capacity){
+        data = new int[capacity];
+        size = 0;
+    }
+
+    public Array(){
+        this(10);
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getCapacity(){
+        return data.length;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void addLast(int e){
+        if(size == data.length){
+            throw new IllegalArgumentException("AddLast failed Array is full");
+        }
+        data[size]=e;
+        size++;
+    }
+
+
+
+
+}
