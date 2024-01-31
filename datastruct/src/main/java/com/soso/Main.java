13ea2ba26411eb9fd39e084ad83a0a0eb38ff7dc
SoSo <sosoariar@163.com>
2024-01-31 20:59:11 +0800

    add test code

--- datastruct/src/main/java/com/soso/Main.java
+++ datastruct/src/main/java/com/soso/Main.java
@@ -2,6 +2,11 @@ package com.soso;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Array arr = new Array(20);
+        for(int i=0;i<10;i++){
+            arr.addLast(i);
+        }
+        System.out.println(arr);
     }
 }
