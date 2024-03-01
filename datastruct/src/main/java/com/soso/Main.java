c347e1c5b87280b7df50528c052b1bad1d9356e6
SoSo <sosoariar@163.com>
2024-03-01 22:23:45 +0800

    Remove-Elements-in-BST

--- datastruct/src/main/java/com/soso/Main.java
+++ datastruct/src/main/java/com/soso/Main.java
@@ -3,40 +3,44 @@ import java.util.Random;
 
 public class Main {
 
+    // 打乱数组顺序
+    private static void shuffle(Object[] arr){
+
+        for(int i = arr.length - 1 ; i >= 0 ; i --){
+            int pos = (int) (Math.random() * (i + 1));
+            Object t = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = t;
+        }
+    }
+
     public static void main(String[] args) {
 
         BST<Integer> bst = new BST<>();
         Random random = new Random();
 
-        int n = 1000;
+        int n = 10000;
 
-        // test removeMin
         for(int i = 0 ; i < n ; i ++)
-            bst.add(random.nextInt(10000));
+            bst.add(random.nextInt(n));
 
-        ArrayList<Integer> nums = new ArrayList<>();
-        while(!bst.isEmpty())
-            nums.add(bst.removeMin());
+        // 注意, 由于随机生成的数据有重复, 所以bst中的数据数量大概率是小于n的
 
-        System.out.println(nums);
-        for(int i = 1 ; i < nums.size() ; i ++)
-            if(nums.get(i - 1) > nums.get(i))
-                throw new IllegalArgumentException("Error!");
-        System.out.println("removeMin test completed.");
+        // order数组中存放[0...n)的所有元素
+        Integer[] order = new Integer[n];
+        for( int i = 0 ; i < n ; i ++ )
+            order[i] = i;
+        // 打乱order数组的顺序
+        shuffle(order);
 
+        // 乱序删除[0...n)范围里的所有元素
+        for( int i = 0 ; i < n ; i ++ )
+            if(bst.contains(order[i])){
+                bst.remove(order[i]);
+                System.out.println("After remove " + order[i] + ", size = " + bst.size() );
+            }
 
-        // test removeMax
-        for(int i = 0 ; i < n ; i ++)
-            bst.add(random.nextInt(10000));
-
-        nums = new ArrayList<>();
-        while(!bst.isEmpty())
-            nums.add(bst.removeMax());
-
-        System.out.println(nums);
-        for(int i = 1 ; i < nums.size() ; i ++)
-            if(nums.get(i - 1) < nums.get(i))
-                throw new IllegalArgumentException("Error!");
-        System.out.println("removeMax test completed.");
+        // 最终整个二分搜索树应该为空
+        System.out.println(bst.size());
     }
 }
