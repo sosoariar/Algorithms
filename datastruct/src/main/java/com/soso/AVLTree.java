d1867a3a75150a809e2455be962d7a3e12297ac9
SoSo <sosoariar@163.com>
2024-03-01 23:44:02 +0800

    Checking-Balancing-and-Binary-Search-Property

--- datastruct/src/main/java/com/soso/AVLTree.java
+++ datastruct/src/main/java/com/soso/AVLTree.java
@@ -33,6 +33,44 @@ public class AVLTree<K extends Comparable<K>, V> {
         return size == 0;
     }
 
+    // 判断该二叉树是否是一棵二分搜索树
+    public boolean isBST(){
+
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+        for(int i = 1 ; i < keys.size() ; i ++)
+            if(keys.get(i - 1).compareTo(keys.get(i)) > 0)
+                return false;
+        return true;
+    }
+
+    private void inOrder(Node node, ArrayList<K> keys){
+
+        if(node == null)
+            return;
+
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+    }
+
+    // 判断该二叉树是否是一棵平衡二叉树
+    public boolean isBalanced(){
+        return isBalanced(root);
+    }
+
+    // 判断以Node为根的二叉树是否是一棵平衡二叉树，递归算法
+    private boolean isBalanced(Node node){
+
+        if(node == null)
+            return true;
+
+        int balanceFactor = getBalanceFactor(node);
+        if(Math.abs(balanceFactor) > 1)
+            return false;
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
     // 获得节点node的高度
     private int getHeight(Node node){
         if(node == null)
@@ -208,6 +246,9 @@ public class AVLTree<K extends Comparable<K>, V> {
             System.out.println("Total different words: " + map.getSize());
             System.out.println("Frequency of PRIDE: " + map.get("pride"));
             System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+
+            System.out.println("is BST : " + map.isBST());
+            System.out.println("is Balanced : " + map.isBalanced());
         }
 
         System.out.println();
