2b54051d20f24e58066d9dd0300b85a8db2f6b38
SoSo <sosoariar@163.com>
2024-03-01 23:03:11 +0800

    Quick-Find

--- datastruct/src/main/java/com/soso/Solution.java
+++ datastruct/src/main/java/com/soso/Solution.java
@@ -1,68 +1,83 @@
-/// 347. Top K Frequent Elements
-/// https://leetcode.com/problems/top-k-frequent-elements/description/
+/// Leetcode 547. Friend Circles
+/// https://leetcode.com/problems/friend-circles/description/
+///
+/// 课程中在这里暂时没有介绍这个问题
+/// 该代码主要用于使用Leetcode上的问题测试我们的UF类
+import java.util.TreeSet;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
+class Solution {
 
-public class Solution {
+    private interface UF {
+        int getSize();
+        boolean isConnected(int p, int q);
+        void unionElements(int p, int q);
+    }
 
-    private class Freq implements Comparable<Freq>{
+    // 我们的第一版Union-Find
+    private class UnionFind1 implements UF {
 
-        public int e, freq;
+        private int[] id;    // 我们的第一版Union-Find本质就是一个数组
 
-        public Freq(int e, int freq){
-            this.e = e;
-            this.freq = freq;
+        public UnionFind1(int size) {
+
+            id = new int[size];
+
+            // 初始化, 每一个id[i]指向自己, 没有合并的元素
+            for (int i = 0; i < size; i++)
+                id[i] = i;
         }
 
-        public int compareTo(Freq another){
-            if(this.freq < another.freq)
-                return -1;
-            else if(this.freq > another.freq)
-                return 1;
-            else
-                return 0;
+        @Override
+        public int getSize(){
+            return id.length;
+        }
+
+        // 查找元素p所对应的集合编号
+        // O(1)复杂度
+        private int find(int p) {
+            if(p < 0 || p >= id.length)
+                throw new IllegalArgumentException("p is out of bound.");
+
+            return id[p];
+        }
+
+        // 查看元素p和元素q是否所属一个集合
+        // O(1)复杂度
+        @Override
+        public boolean isConnected(int p, int q) {
+            return find(p) == find(q);
+        }
+
+        // 合并元素p和元素q所属的集合
+        // O(n) 复杂度
+        @Override
+        public void unionElements(int p, int q) {
+
+            int pID = find(p);
+            int qID = find(q);
+
+            if (pID == qID)
+                return;
+
+            // 合并过程需要遍历一遍所有元素, 将两个元素的所属集合编号合并
+            for (int i = 0; i < id.length; i++)
+                if (id[i] == pID)
+                    id[i] = qID;
         }
     }
 
-    public List<Integer> topKFrequent(int[] nums, int k) {
+    public int findCircleNum(int[][] M) {
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int num: nums){
-            if(map.containsKey(num))
-                map.put(num, map.get(num) + 1);
-            else
-                map.put(num, 1);
-        }
+        int n = M.length;
+        UnionFind1 uf = new UnionFind1(n);
+        for(int i = 0 ; i < n ; i ++)
+            for(int j = 0 ; j < i ; j ++)
+                if(M[i][j] == 1)
+                    uf.unionElements(i, j);
 
-        PriorityQueue<Freq> pq = new PriorityQueue<>();
-        for(int key: map.keySet()){
-            if(pq.size() < k)
-                pq.add(new Freq(key, map.get(key)));
-            else if(map.get(key) > pq.peek().freq){
-                pq.remove();
-                pq.add(new Freq(key, map.get(key)));
-            }
-        }
-
-        LinkedList<Integer> res = new LinkedList<>();
-        while(!pq.isEmpty())
-            res.add(pq.remove().e);
-        return res;
-    }
-
-    private static void printList(List<Integer> nums){
-        for(Integer num: nums)
-            System.out.print(num + " ");
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-
-        int[] nums = {1, 1, 1, 2, 2, 3};
-        int k = 2;
-        printList((new Solution()).topKFrequent(nums, k));
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i = 0 ; i < n ; i ++)
+            set.add(uf.find(i));
+        return set.size();
     }
 }
