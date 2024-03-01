9fc3f133a90a989fa768a1ddf72183eb6a904b4e
SoSo <sosoariar@163.com>
2024-03-01 22:57:58 +0800

    Searching-in-Trie

--- datastruct/src/main/java/com/soso/Main.java
+++ datastruct/src/main/java/com/soso/Main.java
@@ -1,17 +1,47 @@
-/// 该测试用例来源：Leetcode 303. Range Sum Query - Immutable
-/// https://leetcode.com/problems/range-sum-query-immutable/description/
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        System.out.println("Pride and Prejudice");
 
-        SegmentTree<Integer> segTree = new SegmentTree<>(nums,
-                (a, b) -> a + b);
-        System.out.println(segTree);
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words)){
 
-        System.out.println(segTree.query(0, 2));
-        System.out.println(segTree.query(2, 5));
-        System.out.println(segTree.query(0, 5));
+            long startTime = System.nanoTime();
+
+            BSTSet<String> set = new BSTSet<>();
+            for(String word: words)
+                set.add(word);
+
+            for(String word: words)
+                set.contains(word);
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + set.getSize());
+            System.out.println("BSTSet: " + time + " s");
+
+            // ---
+
+            startTime = System.nanoTime();
+
+            Trie trie = new Trie();
+            for(String word: words)
+                trie.add(word);
+
+            for(String word: words)
+                trie.contains(word);
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + trie.getSize());
+            System.out.println("Trie: " + time + " s");
+        }
     }
 }
