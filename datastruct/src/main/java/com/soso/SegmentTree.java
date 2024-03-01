62becb6b64695e07e85bad97b9984431f0c87367
SoSo <sosoariar@163.com>
2024-03-01 22:51:33 +0800

    Query-in-Segment-Tree

--- datastruct/src/main/java/com/soso/SegmentTree.java
+++ datastruct/src/main/java/com/soso/SegmentTree.java
@@ -55,6 +55,37 @@ public class SegmentTree<E> {
         return 2*index + 2;
     }
 
+    // 返回区间[queryL, queryR]的值
+    public E query(int queryL, int queryR){
+
+        if(queryL < 0 || queryL >= data.length ||
+                queryR < 0 || queryR >= data.length || queryL > queryR)
+            throw new IllegalArgumentException("Index is illegal.");
+
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    // 在以treeIndex为根的线段树中[l...r]的范围里，搜索区间[queryL...queryR]的值
+    private E query(int treeIndex, int l, int r, int queryL, int queryR){
+
+        if(l == queryL && r == queryR)
+            return tree[treeIndex];
+
+        int mid = l + (r - l) / 2;
+        // treeIndex的节点分为[l...mid]和[mid+1...r]两部分
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if(queryL >= mid + 1)
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        else if(queryR <= mid)
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+        return merger.merge(leftResult, rightResult);
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
