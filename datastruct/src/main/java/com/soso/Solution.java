80e06a0e795924e796929d3b01dc13c55fb665f1
SoSo <sosoariar@163.com>
2024-03-01 23:03:39 +0800

    Quick-Union

--- datastruct/src/main/java/com/soso/Solution.java
+++ datastruct/src/main/java/com/soso/Solution.java
@@ -13,63 +13,67 @@ class Solution {
         void unionElements(int p, int q);
     }
 
-    // 我们的第一版Union-Find
-    private class UnionFind1 implements UF {
+    // 我们的第二版Union-Find
+    private class UnionFind2 implements UF {
 
-        private int[] id;    // 我们的第一版Union-Find本质就是一个数组
+        // 我们的第二版Union-Find, 使用一个数组构建一棵指向父节点的树
+        // parent[i]表示第一个元素所指向的父节点
+        private int[] parent;
 
-        public UnionFind1(int size) {
+        // 构造函数
+        public UnionFind2(int size){
 
-            id = new int[size];
+            parent = new int[size];
 
-            // 初始化, 每一个id[i]指向自己, 没有合并的元素
-            for (int i = 0; i < size; i++)
-                id[i] = i;
+            // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
+            for( int i = 0 ; i < size ; i ++ )
+                parent[i] = i;
         }
 
         @Override
         public int getSize(){
-            return id.length;
+            return parent.length;
         }
 
-        // 查找元素p所对应的集合编号
-        // O(1)复杂度
-        private int find(int p) {
-            if(p < 0 || p >= id.length)
+        // 查找过程, 查找元素p所对应的集合编号
+        // O(h)复杂度, h为树的高度
+        private int find(int p){
+            if(p < 0 || p >= parent.length)
                 throw new IllegalArgumentException("p is out of bound.");
 
-            return id[p];
+            // 不断去查询自己的父亲节点, 直到到达根节点
+            // 根节点的特点: parent[p] == p
+            while(p != parent[p])
+                p = parent[p];
+            return p;
         }
 
         // 查看元素p和元素q是否所属一个集合
-        // O(1)复杂度
+        // O(h)复杂度, h为树的高度
         @Override
-        public boolean isConnected(int p, int q) {
+        public boolean isConnected( int p , int q ){
             return find(p) == find(q);
         }
 
         // 合并元素p和元素q所属的集合
-        // O(n) 复杂度
+        // O(h)复杂度, h为树的高度
         @Override
-        public void unionElements(int p, int q) {
+        public void unionElements(int p, int q){
 
-            int pID = find(p);
-            int qID = find(q);
+            int pRoot = find(p);
+            int qRoot = find(q);
 
-            if (pID == qID)
+            if( pRoot == qRoot )
                 return;
 
-            // 合并过程需要遍历一遍所有元素, 将两个元素的所属集合编号合并
-            for (int i = 0; i < id.length; i++)
-                if (id[i] == pID)
-                    id[i] = qID;
+            parent[pRoot] = qRoot;
         }
     }
 
     public int findCircleNum(int[][] M) {
 
         int n = M.length;
-        UnionFind1 uf = new UnionFind1(n);
+        UnionFind2 uf = new UnionFind2(n);
         for(int i = 0 ; i < n ; i ++)
             for(int j = 0 ; j < i ; j ++)
                 if(M[i][j] == 1)
