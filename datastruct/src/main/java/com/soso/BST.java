1b08a7975f6eac5afae2eedec7a674246c34bf9f
SoSo <sosoariar@163.com>
2024-03-01 22:20:38 +0800

    PreOrder-Traverse-in-BST

--- datastruct/src/main/java/com/soso/BST.java
+++ datastruct/src/main/java/com/soso/BST.java
@@ -1,14 +1,10 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-
 public class BST<E extends Comparable<E>> {
 
-    private class Node{
+    private class Node {
         public E e;
         public Node left, right;
 
-        public Node(E e){
+        public Node(E e) {
             this.e = e;
             left = null;
             right = null;
@@ -39,7 +35,6 @@ public class BST<E extends Comparable<E>> {
     // 向以node为根的二分搜索树中插入元素e，递归算法
     // 返回插入新节点后二分搜索树的根
     private Node add(Node node, E e){
-
         if(node == null){
             size ++;
             return new Node(e);
@@ -79,154 +74,12 @@ public class BST<E extends Comparable<E>> {
 
     // 前序遍历以node为根的二分搜索树, 递归算法
     private void preOrder(Node node){
-
         if(node == null)
             return;
 
         System.out.println(node.e);
         preOrder(node.left);
         preOrder(node.right);
-    }
-
-    // 二分搜索树的非递归前序遍历
-    public void preOrderNR(){
-
-        Stack<Node> stack = new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            Node cur = stack.pop();
-            System.out.println(cur.e);
-
-            if(cur.right != null)
-                stack.push(cur.right);
-            if(cur.left != null)
-                stack.push(cur.left);
-        }
-    }
-
-    // 二分搜索树的中序遍历
-    public void inOrder(){
-        inOrder(root);
-    }
-
-    // 中序遍历以node为根的二分搜索树, 递归算法
-    private void inOrder(Node node){
-
-        if(node == null)
-            return;
-
-        inOrder(node.left);
-        System.out.println(node.e);
-        inOrder(node.right);
-    }
-
-    // 二分搜索树的后序遍历
-    public void postOrder(){
-        postOrder(root);
-    }
-
-    // 后序遍历以node为根的二分搜索树, 递归算法
-    private void postOrder(Node node){
-
-        if(node == null)
-            return;
-
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.e);
-    }
-
-    // 二分搜索树的层序遍历
-    public void levelOrder(){
-
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            Node cur = q.remove();
-            System.out.println(cur.e);
-
-            if(cur.left != null)
-                q.add(cur.left);
-            if(cur.right != null)
-                q.add(cur.right);
-        }
-    }
-
-    // 寻找二分搜索树的最小元素
-    public E minimum(){
-        if(size == 0)
-            throw new IllegalArgumentException("BST is empty");
-
-        Node minNode = minimum(root);
-        return minNode.e;
-    }
-
-    // 返回以node为根的二分搜索树的最小值所在的节点
-    private Node minimum(Node node){
-        if( node.left == null )
-            return node;
-
-        return minimum(node.left);
-    }
-
-    // 寻找二分搜索树的最大元素
-    public E maximum(){
-        if(size == 0)
-            throw new IllegalArgumentException("BST is empty");
-
-        return maximum(root).e;
-    }
-
-    // 返回以node为根的二分搜索树的最大值所在的节点
-    private Node maximum(Node node){
-        if( node.right == null )
-            return node;
-
-        return maximum(node.right);
-    }
-
-    // 从二分搜索树中删除最小值所在节点, 返回最小值
-    public E removeMin(){
-        E ret = minimum();
-        root = removeMin(root);
-        return ret;
-    }
-
-    // 删除掉以node为根的二分搜索树中的最小节点
-    // 返回删除节点后新的二分搜索树的根
-    private Node removeMin(Node node){
-
-        if(node.left == null){
-            Node rightNode = node.right;
-            node.right = null;
-            size --;
-            return rightNode;
-        }
-
-        node.left = removeMin(node.left);
-        return node;
-    }
-
-    // 从二分搜索树中删除最大值所在节点
-    public E removeMax(){
-        E ret = maximum();
-        root = removeMax(root);
-        return ret;
-    }
-
-    // 删除掉以node为根的二分搜索树中的最大节点
-    // 返回删除节点后新的二分搜索树的根
-    private Node removeMax(Node node){
-
-        if(node.right == null){
-            Node leftNode = node.left;
-            node.left = null;
-            size --;
-            return leftNode;
-        }
-
-        node.right = removeMax(node.right);
-        return node;
     }
 
     @Override
@@ -244,7 +97,7 @@ public class BST<E extends Comparable<E>> {
             return;
         }
 
-        res.append(generateDepthString(depth) + node.e +"\n");
+        res.append(generateDepthString(depth) + node.e + "\n");
         generateBSTString(node.left, depth + 1, res);
         generateBSTString(node.right, depth + 1, res);
     }
