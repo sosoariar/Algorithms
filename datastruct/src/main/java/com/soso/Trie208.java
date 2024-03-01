44e01076a96d7f58cc39e25237950f119e0bf58c
SoSo <sosoariar@163.com>
2024-03-01 22:59:13 +0800

    Prefix-in-Trie

--- datastruct/src/main/java/com/soso/Trie208.java
+++ datastruct/src/main/java/com/soso/Trie208.java
@@ -1,6 +1,9 @@
+/// 208. Implement Trie (Prefix Tree)
+/// https://leetcode.com/problems/implement-trie-prefix-tree/description/
+
 import java.util.TreeMap;
 
-public class Trie {
+public class Trie208 {
 
     private class Node{
 
@@ -18,20 +21,13 @@ public class Trie {
     }
 
     private Node root;
-    private int size;
 
-    public Trie(){
+    public Trie208(){
         root = new Node();
-        size = 0;
-    }
-
-    // 获得Trie中存储的单词数量
-    public int getSize(){
-        return size;
     }
 
     // 向Trie中添加一个新的单词word
-    public void add(String word){
+    public void insert(String word){
 
         Node cur = root;
         for(int i = 0 ; i < word.length() ; i ++){
@@ -40,15 +36,11 @@ public class Trie {
                 cur.next.put(c, new Node());
             cur = cur.next.get(c);
         }
-
-        if(!cur.isWord){
-            cur.isWord = true;
-            size ++;
-        }
+        cur.isWord = true;
     }
 
     // 查询单词word是否在Trie中
-    public boolean contains(String word){
+    public boolean search(String word){
 
         Node cur = root;
         for(int i = 0 ; i < word.length() ; i ++){
@@ -61,11 +53,11 @@ public class Trie {
     }
 
     // 查询是否在Trie中有单词以prefix为前缀
-    public boolean isPrefix(String prefix){
+    public boolean startsWith(String isPrefix){
 
         Node cur = root;
-        for(int i = 0 ; i < prefix.length() ; i ++){
-            char c = prefix.charAt(i);
+        for(int i = 0 ; i < isPrefix.length() ; i ++){
+            char c = isPrefix.charAt(i);
             if(cur.next.get(c) == null)
                 return false;
             cur = cur.next.get(c);
