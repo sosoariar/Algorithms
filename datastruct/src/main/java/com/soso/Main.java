cc5715bcfe2fe0a9cabea4b4b6716aadcb1c5d86
SoSo <sosoariar@163.com>
2024-03-01 22:27:20 +0800

    Set-Basics-and-BSTSet

--- datastruct/src/main/java/com/soso/Main.java
+++ datastruct/src/main/java/com/soso/Main.java
@@ -1,46 +1,34 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main {
 
-    // 打乱数组顺序
-    private static void shuffle(Object[] arr){
-
-        for(int i = arr.length - 1 ; i >= 0 ; i --){
-            int pos = (int) (Math.random() * (i + 1));
-            Object t = arr[pos];
-            arr[pos] = arr[i];
-            arr[i] = t;
-        }
-    }
-
     public static void main(String[] args) {
 
-        BST<Integer> bst = new BST<>();
-        Random random = new Random();
+        System.out.println("Pride and Prejudice");
 
-        int n = 10000;
+        ArrayList<String> words1 = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
 
-        for(int i = 0 ; i < n ; i ++)
-            bst.add(random.nextInt(n));
+            BSTSet<String> set1 = new BSTSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
 
-        // 注意, 由于随机生成的数据有重复, 所以bst中的数据数量大概率是小于n的
+        System.out.println();
 
-        // order数组中存放[0...n)的所有元素
-        Integer[] order = new Integer[n];
-        for( int i = 0 ; i < n ; i ++ )
-            order[i] = i;
-        // 打乱order数组的顺序
-        shuffle(order);
 
-        // 乱序删除[0...n)范围里的所有元素
-        for( int i = 0 ; i < n ; i ++ )
-            if(bst.contains(order[i])){
-                bst.remove(order[i]);
-                System.out.println("After remove " + order[i] + ", size = " + bst.size() );
-            }
+        System.out.println("A Tale of Two Cities");
 
-        // 最终整个二分搜索树应该为空
-        System.out.println(bst.size());
+        ArrayList<String> words2 = new ArrayList<>();
+        if(FileOperation.readFile("a-tale-of-two-cities.txt", words2)){
+            System.out.println("Total words: " + words2.size());
+
+            BSTSet<String> set2 = new BSTSet<>();
+            for(String word: words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
     }
 }
